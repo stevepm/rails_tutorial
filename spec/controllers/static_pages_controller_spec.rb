6b96ca005d7637ef155ea1cb19d191d918fc15dd
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-02-05 15:04:38 -0800

    Add contacts action to static_pages_controller

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -23,4 +23,11 @@ RSpec.describe StaticPagesController, type: :controller do
     end
   end
 
+  describe "GET #contact" do
+    it 'returns http success' do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
