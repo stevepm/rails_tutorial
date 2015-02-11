31542668833ef2808835d94713691ab2f083e698
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-02-11 12:50:52 -0800

    Add users index page and style it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def index
-
+    @users = User.all
   end
-  
+
   def new
     @user = User.new
   end
